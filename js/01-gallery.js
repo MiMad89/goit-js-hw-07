0600645799ba218fea535319378b99dc39f60f41
MiMad89 <michal9977@gmail.com>
2023-03-22 13:46:54 +0100

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((img) => {
+  galleryEl.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>`
+  );
+});
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"/>`
+  );
+  instance.show();
+});
 
 console.log(galleryItems);
